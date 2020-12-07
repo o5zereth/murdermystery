b8e27cb5ab845e3dbf8ed6189a45c362e3edea03
o5zereth <jthomaspiper2006@gmail.com>
2020-12-07 15:37:09 -0500

    fix percent not actually being percent

--- Config.cs
+++ Config.cs
@@ -12,25 +12,25 @@ namespace MurderMystery
         [Description("Specifies the time (in seconds) to give out player equipment when the round starts. Must be atleast 10 seconds. (Recommend atleast 45)")]
         public float EquipmentCooldown { get; set; } = 60f;
         [Description("Specifies the murderer percentage. Must be between zero and one.")]
-        public float MurdererPercent { get; set; } = 1 / 6;
+        public float MurdererPercent { get; set; } = 1f / 6f;
         [Description("Specifies the detective percentage. Must be between zero and one.")]
-        public float DetectivePercent { get; set; } = 1 / 12;
+        public float DetectivePercent { get; set; } = 1f / 12f;
 
         public void Validate()
         {
             if (EquipmentCooldown < 10f) { EquipmentCooldown = 10f; }
             if (MurdererPercent <= 0 || MurdererPercent >= 1)
             {
-                MurdererPercent = 1 / 6;
+                MurdererPercent = 1f / 6f;
             }
-            if (DetectivePercent <= 0 || DetectivePercent >= 1)
+            if (DetectivePercent <= 0f || DetectivePercent >= 1f)
             {
-                DetectivePercent = 1 / 12;
+                DetectivePercent = 1f / 12f;
             }
-            if (MurdererPercent + DetectivePercent >= 1)
+            if (MurdererPercent + DetectivePercent >= 1f)
             {
-                MurdererPercent = 1 / 6;
-                DetectivePercent = 1 / 12;
+                MurdererPercent = 1f / 6f;
+                DetectivePercent = 1f / 12f;
             }
         }
     }
--- README.md
+++ README.md
@@ -13,5 +13,5 @@
 | IsEnabled | bool | Enables the plugin. | true |
 | RequireRoundRestart | bool | Specifies if the gamemode should require a round restart to begin. | true |
 | EquipmentCooldown | float | Specifies the time (in seconds) to give out player equipment when the round starts. Must be atleast 10 seconds. (Recommend atleast 45) | 60f |
-| MurdererPercent | float | Specifies the murderer percentage. Must be between zero and one. | 1 / 6 |
-| DetectivePercent | float | Specifies the detective percentage. Must be between zero and one. | 1 / 12 |
+| MurdererPercent | float | Specifies the murderer percentage. Must be between zero and one. | 1f / 6f |
+| DetectivePercent | float | Specifies the detective percentage. Must be between zero and one. | 1f / 12f |
